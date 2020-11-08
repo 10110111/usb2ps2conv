fef5012dda1705d83e0131491f3eab3faa173de3
Ruslan Kabatsayev <b7.10110111@gmail.com>
2020-11-08 14:43:07 +0300

    Add an option to disable LED status indication

--- src/led.c
+++ src/led.c
@@ -1,8 +1,9 @@
 #include "stm32f4xx_hal_conf.h"
 #include "led.h"
 
-#define	LED_GPIO_PORT           GPIOD
-#define	LED_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOD_CLK_ENABLE()
+#ifdef ENABLE_LED
+# define	LED_GPIO_PORT           GPIOD
+# define	LED_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOD_CLK_ENABLE()
 
 static const uint16_t ledPins[] =
 {
@@ -11,29 +12,39 @@ static const uint16_t ledPins[] =
     GPIO_PIN_14,
     GPIO_PIN_15,
 };
+#endif
 
 void ledOn(const LED_ID led)
 {
+#ifdef ENABLE_LED
     LED_GPIO_PORT->BSRR = ledPins[led];
+#endif
 }
 
 void ledOff(const LED_ID led)
 {
+#ifdef ENABLE_LED
     LED_GPIO_PORT->BSRR = (uint32_t)ledPins[led] << 16;
+#endif
 }
 
 void ledsOn(void)
 {
+#ifdef ENABLE_LED
     LED_GPIO_PORT->BSRR = ledPins[0]|ledPins[1]|ledPins[2]|ledPins[3];
+#endif
 }
 
 void ledsOff(void)
 {
+#ifdef ENABLE_LED
     LED_GPIO_PORT->BSRR = (uint32_t)(ledPins[0]|ledPins[1]|ledPins[2]|ledPins[3]) << 16;
+#endif
 }
 
 void ledInit(void)
 {
+#ifdef ENABLE_LED
     LED_GPIO_CLK_ENABLE();
 
     GPIO_InitTypeDef init =
@@ -44,4 +55,5 @@ void ledInit(void)
         .Speed = GPIO_SPEED_LOW,
     };
     HAL_GPIO_Init(LED_GPIO_PORT, &init);
+#endif
 }
